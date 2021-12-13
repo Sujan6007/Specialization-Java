ad52fd89a5960b5f950ae87b03dc8c5efb2999ab
Sujan6007 <90595888+Sujan6007@users.noreply.github.com>
2021-12-13 18:08:50 +0530

    Add files via upload

---
+++ Day16/SpringCore/src/main/java/com/app/spring/emp/Address.java
@@ -0,0 +1,12 @@
+package com.app.spring.emp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+	Employee emp;
+	public Address(Employee emp) {
+		/* emp.details(); */
+	}
+
+}
