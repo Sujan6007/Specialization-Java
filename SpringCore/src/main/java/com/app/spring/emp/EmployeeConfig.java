0e23b20a353371e9a9f42f87c93a147d544b6ac6
Sujan6007 <90595888+Sujan6007@users.noreply.github.com>
2021-12-13 18:05:50 +0530

    Add files via upload

---
+++ SpringCore/src/main/java/com/app/spring/emp/EmployeeConfig.java
@@ -0,0 +1,10 @@
+package com.app.spring.emp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.app.spring.emp")
+public class EmployeeConfig {
+
+}
